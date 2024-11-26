5dfc0972ecbed5a3f5673b874834eb196e9b3d87
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-11-26 10:46:15 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,82 @@
+# x = 1
+# y = 2
+# print(x,y)
+
+# print(y)
+
+
+# +,-,*,/
+
+# x= 1
+# y =2
+# print(x*y)
+
+# single line comment
+# dffjinj
+# vjdvjkdff
+# vdvnjnk
+
+"""YUDGFYHEGFGED
+JBDJVBNHJDBNVJL
+DVBDVJKN"""
+
+# x = int(input('enter your number : '))
+
+# y = int(input('enter your number : '))
+
+# print(x+y)
+
+
+# variables
+
+# x = 1
+# print(x)
+
+
+# String 
+
+
+# x = "hello"
+# print(x)
+
+
+# x = 'hello'
+# print(x)
+# print(type(x))
+# True variables
+
+# x = 1
+# x1 = 23
+# hello = 2
+# x_x = 34
+
+# Flase variables
+
+# 1x = 1
+# x.21 = 45
+
+
+# hello = "dhyey"
+# demo = 'vithani'
+# print(hello+demo)
+
+
+# flote
+# x = 1.2
+# print(x)
+# print(type(x))
+
+x = 10.3
+y = 23.6
+
+print(x+y)
+
+
+# int
+# String
+# float
+# bool
+# list
+# tuple
+# sets
+# complex
---
+++ second.py
@@ -0,0 +1,47 @@
+"""x = 1
+y = 2
+
+# print(x)
+# print(y)
+# print(x,y)
+
+print(x+y)
+print(y-x)
+print(x*y)
+print(x/y)
+
+x = int(input('enter the number : '))
+y = int(input('enter the number : '))
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)"""
+
+#true variable
+x=1
+y1= "hello"
+_a = 5.5
+number =7
+ 
+# false variable
+#1x= 1 
+# #x =5
+
+print(type(x))
+print(type(y1))
+print(type(_a))
+
+#float
+a = 8
+b = 16.0
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)
+
+#String
+name = "dhyey"
+lastname = 'vithani'
+print(name+lastname)
+print(name,lastname)
