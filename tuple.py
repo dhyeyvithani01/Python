e5d9d14c36582e4cfd5e49ad6e30ad035e892c68
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-11-28 15:08:43 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+"""dictonary"""
+x ={1:2,"list":[1,2.5,"name",0],3:'hello',}
+print(x)
+y = x.keys()
+print(y)
+z = x.values()
+print(z)
+print(type(x),type(y),type(z))
+x.clear()
+print(x)
+print(type(x))
+
+"""tuple"""
+x = (1,2,5.6,'dhyey')
+print(x)
+print(type(x))
+print(x[1])
