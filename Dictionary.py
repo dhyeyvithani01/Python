ea245eee51b0bc1dd354894143a47b4d3aa69e81
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-11-29 12:02:25 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,51 @@
+"""dictonary"""
+# x ={1:2,"list":[1,2.5,"name",0],3:'hello',}
+# print(x)
+# y = x.keys()
+# print(y)
+# z = x.values()
+# print(z)
+# print(type(x),type(y),type(z))
+# x.clear()     
+# print(x)     
+# print(type(x))     
+# x ={1:2,"list":[1,2.5,"name",0],3:'hello',}
+# x.clear()
+# print(x)
+# y = x.copy()
+# print(y)
+# y = dict.fromkeys(x)
+# z = dict.fromkeys(x,[1,2.5,'hello'])
+# print(y)
+# print(z)
+# print(x.get(3))
+# print(x.get('name'))
+# print(y.items())
+# print(y.keys())
+# print(y.values())
+# z.pop(3)
+# print(z)
+# print(x.popitem())
+# x.setdefault(4,5.2)
+# print(x)
+
+# a = {"A":5,"B":7.2}
+# a.update(B=8,C="name")
+# print(a)
+
+
+     
+"""tuple"""     
+# x = (1,2,5.6,'dhyey')     
+# print(x)     
+# print(type(x))     
+# print(x[1])     
+
+
+"""sets"""
+
+# x = {1,2,3,4,5,6,'hello',8,89,4.6,998,566,76,34,56,5,89}
+# print(x)
+# print(type(x))
+
+
