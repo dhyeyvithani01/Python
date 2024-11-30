af792ef3937ea7fd660dd867328964913b3e683c
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-11-30 11:28:47 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,47 @@
+"""Arithmetic Operators"""
+# print(5/2)
+# print(-5/2)
+# print(-5/-2)
+# print(-5.0/2)
+# print(-5/2.0)
+# a = 9
+# b = 9
+# c = a**b
+# print(c)
+
+# a = 2
+# b = 5
+# c = 9
+
+"""Comparison Operators"""
+
+# print(a<b)
+# print(a>b)
+# print(a==c)
+# print(a!=b)
+# print(a>=b)
+# print(b<16<b**3!=125)
+
+"""Assignment Operators"""
+
+# x = 3 
+# y = 1
+# x+=5
+# print(x)
+# x-=10
+# print(x)
+# x*=-10
+# print(x)
+# y+=y
+# print(y)
+# x=21
+# x/=y
+# print(x)
+
+# p = 21
+# q = 2
+# p%=q
+# print(p)
+# p+=1
+# p**=q
+# print(p)
