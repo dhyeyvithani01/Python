c0ac3e0cc93076cd6741d4b8cd72040fe7922202
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-02 11:43:35 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,27 @@
+# if 5<3:
+#     print("right")
+# elif 10>45:
+#     print("rightt")
+# elif 5==23:
+#     print("dfghjk")
+# elif 5!=5:
+#     print("qwtyui")
+# else:
+#     print("wrong")
+
+# <,>,==,!=,<=,>=
+
+print("grade:")
+x = 93.50
+if x==0:
+    print('Enter Valid marks')
+elif x>=90:
+    print('A1')
+elif x>80:
+    print('A')
+elif x>60:
+    print('B')
+elif x>35:
+    print('C')
+else:
+    print('F')
