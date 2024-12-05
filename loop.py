f9170fe2749f43b5f50f1f763e1a310a93b92e41
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-05 10:46:22 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,10 @@
-count = 0
+# count = 0
 
-while (count > -1):
-    count = count + 1
-    print(count)
+# while (count > -1):
+#     count = count + 1
+#     print(count)
 
+# for loop 
+x = 4
+for x in range(100):
+    print(x)
