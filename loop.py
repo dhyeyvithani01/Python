f5b178ddbf87d3ea785b6092ae3a974e1c30be29
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-06 15:35:59 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -26,10 +26,10 @@
 
 
     
-print("\nDictionary Iteration")
-d = dict({'x':123, 'y':354})
-for i in d:
-    print("%s  %d" % (i, d[i]))
+# print("\nDictionary Iteration")
+# d = dict({'x':123, 'y':354})
+# for i in d:
+#     print("%s  %d" % (i, d[i]))
  
 """"odd-even"""
 # x = int(input("Enter any number:"))
