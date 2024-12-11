366f22e6d0e9170f5e3818e81d330a366875cff1
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-11 11:15:39 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,8 +1,8 @@
-x = [1,2,3,0,'a','dhyey7',1.7,0,1,2,1,True]
-print(x)
+# x = [1,2,3,0,'a','dhyey7',1.7,0,1,2,1,True]
+# print(x)
 
-x.append(4)
-print(x)
+# x.append(4)
+# print(x)
 
 # x.clear()
 # print(x)
@@ -10,47 +10,56 @@ print(x)
 # x.copy()
 # print(x)
 
-y = x.count(1)
-z = x.count(2)
-p = x.count(0)
-print(y,z,p)
+# y = x.count(1)
+# z = x.count(2)
+# p = x.count(0)
+# print(y,z,p)
 
-cars = ['Bmw','maruti']
-more_cars =['tata']
-print(cars)
+# cars = ['Bmw','maruti']
+# more_cars =['tata']
+# print(cars)
 # cars.append(more_cars)
 # print(cars)
-cars.extend(more_cars)
-print(cars)
+# cars.extend(more_cars)
+# print(cars)
 
-# # list of animals
+# list of animals
 # Animals= ["cat", "dog", "tiger",'hhhh']
-# # searching positiion of dog
+# searching positiion of dog
 # print(Animals.index("hhhh"))
 
 # a =[1,2,3,5,7]
-# # print(a.index(7))
-# # print(a.index(2,0,3))
+# print(a.index(7))
+# print(a.index(2,0,3))
 
-a =[1,2,3,5,7,1,2]
-a.insert(3,15)
-print(a)
+# a =[1,2,3,5,7,1,2]
+# a.insert(3,15)
+# print(a)
 
-b = [3,5,7,14,9]
-print(b)
-b.pop(2)
-print(b)
+# b = [3,5,7,14,9]
+# print(b)
+# b.pop(2)
+# print(b)
 
-c = [1,2,3,34,5,5,1,3,2]
-print(c)
-c.remove(34)
-print(c)
-c.remove(1)
-print(c)
+# c = [1,2,3,34,5,5,1,3,2]
+# print(c)
+# c.remove(34)
+# print(c)
+# c.remove(1)
+# print(c)
 
-c.reverse()
-print(c)
+# c.reverse()
+# print(c)
 
-d = [1,5,2,6,3,8,4]
-d.sort(reverse=True)
-print(d)
+# d = [1,5,2,6,3,8,4]
+# d.sort(reverse=True)
+# print(d)
+
+x,y,z=1,[1,2,3.5,[25,'name',[2,5,7]]],[10,20,0,["hyy",7],'10.5',"world"]
+print(z)
+print(z[1:2])
+print(z[-4:-1])
+print(z[-1:-4:-1])
+print(z[5:2:-1])
+print(z[1],z[3])
+print(y[3][2][2])
