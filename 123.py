06ca4d3d8d8f3f23373c409fc354a66536c3c53b
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-14 11:43:34 +0530

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,1 +1,11 @@
-print("dfjhbdjfbj")
+print("Divisibility checker")
+x = int(input("Enter a number: "))
+y = int(input("Enter a second number: "))
+while x>0 and y>0:
+        if x//y==x/y:
+            print(f"{x} is dividable by {y}")
+        else:
+            print(f"{x} is not dividable by {y}")
+        break
+else:
+    print("Enter a positive integer!")
