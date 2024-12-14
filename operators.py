c6b817159956d5cbd61f69e967bf1ac0eb86a29b
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-14 11:01:57 +0530

    Update operators.py

--- operators.py
+++ operators.py
@@ -4,13 +4,13 @@
 # print(-5/-2)
 # print(-5.0/2)
 # print(-5/2.0)
-# a = 9
+# a = 17
 # b = 9
-# c = a**b
+# c = a//b
 # print(c)
 
 # a = 2
-# b = 5
+# b = 5 
 # c = 9
 
 """Comparison Operators"""
@@ -45,3 +45,47 @@
 # p+=1
 # p**=q
 # print(p)
+
+
+
+# logical operatours
+
+# x = 10
+# y = 10
+
+# print(x == 18 or not x<y or x==y)
+
+
+# identity
+
+# print(x is y,x==y)
+# print(x is not y,x!=y)
+
+
+
+# member ship 
+
+
+
+# x = "Hello "
+# print('H' in x)
+
+
+
+
+# x = 11
+# y =567
+
+# print(bin(x))
+# print(bin(y))
+# print(bin(x^y))
+
+
+# Python program to illustrate the use
+# of 'is' and '==' operators
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+
+# using 'is' and '==' operators
+# print(a in b)
+# print(a == b)
