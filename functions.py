f2d9f66e7150f52d95b0982fd18869956406e7f0
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-14 12:10:45 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,12 @@
 # A simple Python function
-def fun():
-    print("Welcome to GFG")
+def odd_even():
+    number = int(input("Enter any number:"))
+    if number%2==0:
+        print("The given number is Even.")
+    else:
+        print("The given number is Odd.")
+
 
 
 # Driver code to call a function
-fun()
+odd_even()
