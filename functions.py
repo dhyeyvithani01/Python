b4c1f5bdea8d2c36e976eaeb3bf4b9ec9f7d12cc
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-16 12:17:19 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,20 @@
 # A simple Python function
-def odd_even():
-    number = int(input("Enter any number:"))
-    if number%2==0:
-        print("The given number is Even.")
-    else:
-        print("The given number is Odd.")
+# def odd_even():
+#     number = int(input("Enter any number:"))
+#     if number%2==0:
+#         print("The given number is Even.")
+#     else:
+#         print("The given number is Odd.")
 
 
 
-# Driver code to call a function
-odd_even()
+# # Driver code to call a function
+# odd_even()
+
+
+
+# def hello():
+#     print('hello world')
+# hello()
+
+
