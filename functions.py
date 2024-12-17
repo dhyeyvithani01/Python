506a2a9aceae909f2eb2e21c7b0bc4e31429c78b
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-17 11:04:04 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,3 +20,17 @@
 # # inbilt function 
 # print('hello')
 # hello(10)
+
+"""Divisibility Checker"""
+def Divisibilty_Check():
+    a=1
+    while a!=0:
+        x = int(input("Enter a first number:"))
+        y = int(input("Enter a second number:"))
+        if x//y == x/y:
+            print("The {x} is dividable by {y}.") 
+        else:
+            print("The {x} is dividable by {y}.")
+        a = int(input("Enter 0 to end program:\nEnter 1 to run again program:\n"))
+    else:
+        print("Program ended.")
