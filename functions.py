5b0df179c5011710985b688ed54f721cbaec4894
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-18 12:11:04 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -60,14 +60,22 @@ def Phibonachi():
         print(a)
         a, b = b, a + b
 
-A=int(input("print 1 to run Divisibilty_Check() program.\nprint 2 to run Result() program.\nprint 3 to run Compare_value().\nprint 4 to run odd_even() program.\nprint 5 to Phibonachi() program.:\n"))
-if A == 1:
-    Divisibilty_Check()
-elif A==2:
-    Result()
-elif A==3:
-    Compare_value()
-elif A==4:
-    odd_even()
-elif A==5:
-    Phibonachi()
+# A=int(input("print 1 to run Divisibilty_Check() program.\nprint 2 to run Result() program.\nprint 3 to run Compare_value().\nprint 4 to run odd_even() program.\nprint 5 to Phibonachi() program.:\n"))
+# if A == 1:
+#     Divisibilty_Check()
+# elif A==2:
+#     Result()
+# elif A==3:
+#     Compare_value()
+# elif A==4:
+#     odd_even()
+# elif A==5:
+#     Phibonachi()
+
+
+num =5
+def hellos():
+    print(num)
+    
+hellos()
+print(num)
