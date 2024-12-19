d30d9631e5f51836046e93d227e6d629337c4389
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-19 12:04:46 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -89,3 +89,18 @@ def Phibonachi():
 # for i in name:
 #     if len(i)==3:
 #         print(i)
+
+"""is_prime()"""
+def is_prime(n):
+    if n in [2, 3]:
+        return True
+    if (n == 1) or (n % 2 == 0):
+        return False
+    r = 3
+    while r * r <= n:
+        if n % r == 0:
+            return False
+        r += 2
+    return True
+n=int(input("Enter any number to if it is prime or not:"))
+print(is_prime(n))
