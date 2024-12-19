94e58850b4ffa03c21ead906d858397dbb7840dc
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-19 12:05:31 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -90,17 +90,17 @@ def Phibonachi():
 #     if len(i)==3:
 #         print(i)
 
-"""is_prime()"""
-def is_prime(n):
-    if n in [2, 3]:
-        return True
-    if (n == 1) or (n % 2 == 0):
-        return False
-    r = 3
-    while r * r <= n:
-        if n % r == 0:
-            return False
-        r += 2
-    return True
-n=int(input("Enter any number to if it is prime or not:"))
-print(is_prime(n))
+# """is_prime()"""
+# def is_prime(n):
+#     if n in [2, 3]:
+#         return True
+#     if (n == 1) or (n % 2 == 0):
+#         return False
+#     r = 3
+#     while r * r <= n:
+#         if n % r == 0:
+#             return False
+#         r += 2
+#     return True
+# n=int(input("Enter any number to if it is prime or not:"))
+# print(is_prime(n))
