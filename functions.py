4007a8ac40fb8e157167dffae277825f54d59bfc
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-27 11:00:34 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -167,19 +167,19 @@ def Phibonachi():
 # print(value[3])
 
 
-def is_palindrome(s):
-    s = s.lower()
-    left = 0
-    right = len(s) - 1
-    while left < right:
-        if s[left] != s[right]:
-            return False
-        left += 1
-        right -= 1
-    return True
-input_string = input("Enter a string: ")
-if is_palindrome(input_string):
-    print(f'"{input_string}" is a palindrome!')
-else:
-    print(f'"{input_string}" is not a palindrome.')
+# def is_palindrome(s):
+#     s = s.lower()
+#     left = 0
+#     right = len(s) - 1
+#     while left < right:
+#         if s[left] != s[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
+# input_string = input("Enter a string: ")
+# if is_palindrome(input_string):
+#     print(f'"{input_string}" is a palindrome!')
+# else:
+#     print(f'"{input_string}" is not a palindrome.')
 
