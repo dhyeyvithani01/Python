5c51c054e01d2d8fbf48a073ee05b34adb754df4
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-27 11:41:39 +0530

    Update KBC.py

---
+++ KBC.py
@@ -0,0 +1,22 @@
+Question = [
+    [
+        "Which of these is a primary color?","Green","Red","Purple","Orange","B"
+    ],
+    [
+        "Who wrote the famous book 'Harry Potter and the Sorcerer's Stone?","J.R.R. Tolkien","J.K. Rowling","George R.R. Martin","C.S. Lewis","B"
+    ],
+    [
+        "In which country would you find the historical site of Machu Picchu?","Brazil","Peru","Mexico","Ecuador","B"
+    ],
+    [
+        "What is the capital city of Canada?","Vancouver","Torrento","Ottawa","Montreal","C"
+    ],
+    [
+        "Who is the author of the book 'The God of Small Things'?","Amitav Ghosh","Vikram Seth","Kiran Desai","Arundhati Roy","D"
+    ],
+    [
+        "In which year did India launch its first mission to Mars, called 'Mangalyaan'?","2013","2009","2014","2019","A"
+    ]
+]
+
+Level = [10000,20000,40000,80000,160000,320000,640000,1200000]
