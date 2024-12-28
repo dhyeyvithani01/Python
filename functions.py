5e9daa681f4616fc0708120a20043692a76074bb
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-28 15:09:29 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -221,26 +221,50 @@ def Phibonachi():
 
 
 """Guessing game"""
+
 # import random
-# x = random.randrange(1,100)
-# y = 0
-# a = 1
-# while a<=5:
-#     while y!=x and y<=100 and a<=5:
-#         y = int(input("Guess any number between 1 and 100:"))
-#         if y==x:
-#             print("Congratulations!!! You guessed the correct number.")
+
+# def guessing_game():
+#     # Randomly generate a number between 1 and 100
+#     number_to_guess = random.randint(1, 100)
+    
+#     # Set the maximum number of attempts
+#     max_attempts = 10
+#     attempts_left = max_attempts
+    
+#     print("Welcome to the Guessing Game!")
+#     print("I'm thinking of a number between 1 and 100.")
+#     print(f"You have {max_attempts} attempts to guess the number.")
+
+#     # Start the guessing loop
+#     while attempts_left > 0:
+#         guess = int(input(f"\nYou have {attempts_left} attempts left. Enter your guess: "))
+        
+#         # Check if the guess is correct
+#         if guess == number_to_guess:
+#             print("Congratulations! You guessed the number correctly!")
 #             break
-#         elif y>x and (y-x)>20:
-#             print("the number is too small.")
-#         elif y>x and (y-x)<=20:
-#             print("the number is small.")
-#         elif y<x and (y-x)>20:
-#             print("the number is too big.")
-#         elif y<x and (y-x)<=20:
-#             print("the number is big.")
-#         print(f"You have only {5-a} attemps left.")
-#         a+=1
-#     else:
-#         print("Enter the number between 1 and 100.")
+        
+#         # Provide hints for high or low guesses
+#         if guess < number_to_guess:
+#             print("Too low!")
+#         else:
+#             print("Too high!")
+
+#         # Offer additional hints based on how close the guess is
+#         if attempts_left <= 5:  # Give closer hints after 5 attempts
+#             difference = abs(number_to_guess - guess)
+#             if difference <= 10:
+#                 print("You're within 10 of the number!")
+#             if difference <= 5:
+#                 print("You're very close! Within 5 of the number!")
+        
+#         attempts_left -= 1
+
+#     # If the player runs out of attempts
+#     if attempts_left == 0:
+#         print(f"Sorry, you're out of attempts. The number was {number_to_guess}.")
+
+# # Run the game
+# guessing_game()
 
