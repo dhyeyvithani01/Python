13ae04fa79358eac3d930e4bd5414aea6ced4f47
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-30 15:25:41 +0530

    Update Built_inFun.py

---
+++ Built_inFun.py
@@ -0,0 +1,10 @@
+"""Understanding paratmeters in bulit-in python functions"""
+
+# print function
+# sep="" is used for seperating objects and variable
+# end="" is used for ending function with certain string
+print("Hello","Dhyey",sep="------",end="\n\n\n")
+print("Hello","Dhyey",sep="******",end="\n**\n")
+print("Hello","Dhyey",sep="______",end="\n\n")
+
+# input function
