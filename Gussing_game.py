7b4924ac5e98a9b1669b7a3b6153d9b35eaa2508
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-30 11:32:06 +0530

    Update Gussing_game.py

--- Gussing_game.py
+++ Gussing_game.py
@@ -23,3 +23,43 @@ import random
 def Guessing_game():
     #generating number between 1 and 100
     x =random.randrange(1,100)
+    Max_attempts=10
+    y=0
+    a=0
+
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print(f"You have {Max_attempts} attempts to guess the number.")
+
+    
+    while y!=x and y>=0 and y<=100 and a<=Max_attempts:
+        y = int(input("Guess the number:"))
+        if y>=0 and y<=100:
+            if y==x:
+                print("Congratulations!!! You guessed the correct number.")
+                break
+            elif y>x:
+                if (y-x)>=20:
+                    print("The number is too small.")
+                if (y-x)<20:
+                    print("The number is small.")
+                if (y-x)<5:
+                    print("You're very close, within 5 numbers.")
+            elif y<x:
+                if (x-y)>=20:
+                    print("The number is too big.")
+                if (x-y)<20:
+                    print("The number is big.")
+                if (x-y)<5:
+                    print("You're very close, within 5 numbers.")
+            a+=1
+            if a!=0:
+                print(f"You have only {Max_attempts-a} attempts left.")
+            else:
+                print(f"Sorry, you're out of attempts. The number was {y}.")
+        else:
+            print("Enter the number between 1 and 100.")
+    
+
+
+Guessing_game()
