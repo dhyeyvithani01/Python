450e63e853ca7b2f7e4fa6416b80dfbc473d847d
dhyeyvithani01 <dhyey1121.7@gmail.com>
2024-12-31 15:05:37 +0530

    Update Built_inFun.py

--- Built_inFun.py
+++ Built_inFun.py
@@ -1,14 +1,12 @@
 # """Understanding paratmeters in bulit-in python functions"""
 
-# # print function
-# # sep="" is used for seperating objects and variable
-# # end="" is used for ending function with certain string
-# print("Hello","Dhyey",sep="------",end="\n\n\n")
-# print("Hello","Dhyey",sep="******",end="\n**\n")
-# print("Hello","Dhyey",sep="______",end="\n\n")
-
-# # input function
-
-
-
-
+# print function
+# sep="" is used for seperating objects and variable
+# end="" is used for ending function with certain string
+print("Hello","Dhyey",sep="------",end="\n\n\n")
+print("Hello","Dhyey",sep="******",end="\n**\n")
+print("Hello","Dhyey",sep="______",end="\n\n")
+# Removes the spaces from left and right side of string
+x = input("Enter the name:")
+x = x.strip()
+print(x)
