b458bc045b52904cc7e9f47a0e058a9660a2ca17
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-02 15:28:36 +0530

    Update second.py

--- second.py
+++ second.py
@@ -57,13 +57,19 @@ print(x/y)"""
 # for First_name in students:
 #     print(First_name,students[First_name])
 
-# Data = [
-#     {"Name":"Harry","Age":"25","Edu":"BCA","Work":"Softwear Developer","Salary":"100000"},
-#     {"Name":"Jack","Age":"36","Edu":"B.Sc","Work":"physicist","Salary":"500000"},
-#     {"Name":"Ramcharan","Age":"41","Edu":"B.tech","Work":"Chemical Engineer","Salary":"300000"},
-#     {"Name":"james","Age":"55","Edu":"BA","Work":"Proffesor","Salary":"250000"},
-#     {"Name":"Sam","Age":"23","Edu":"M.tech","Work":"A.I. Reseacher","Salary":"1000000"}
-# ]
+Data = [
+    {"Name":"Harry","Age":"25","Edu":"BCA","Work":"Softwear Developer","Salary":"100000"},
+    {"Name":"Jack","Age":"36","Edu":"B.Sc","Work":"physicist","Salary":"500000"},
+    {"Name":"Ramcharan","Age":"41","Edu":"B.tech","Work":"Chemical Engineer","Salary":"300000"},
+    {"Name":"james","Age":"55","Edu":"BA","Work":"Proffesor","Salary":"250000"},
+    {"Name":"Sam","Age":"23","Edu":"M.tech","Work":"A.I. Reseacher","Salary":"1000000"}
+]
 # for i in Data:
 #     print(i["Name"],i["Age"],i["Edu"],i["Work"],i["Salary"],sep=", ")
 #     print()
+
+# print(Data[0])
+# print(Data[1])
+# print(Data[2])
+# print(Data[3])
+# print(Data[4])
