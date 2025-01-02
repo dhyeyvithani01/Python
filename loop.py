20ba7286046e2c55b4a918b09930b90fcd38a596
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-02 11:25:41 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -332,3 +332,13 @@
 #     for j in range(1,i+1):
 #         print(j,end=" ")
 #     print()
+"""pattern-24"""
+# x="Python"
+# for i in range(1,7):
+#     for j in range(i):
+#         print(x[j],end="")
+#     print()
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(x[j],end="")
+#     print()
