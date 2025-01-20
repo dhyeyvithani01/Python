6568f8d29f994487920d4f2d499080c4144227fa
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-20 16:34:06 +0530

    Update say.py

--- say.py
+++ say.py
@@ -1,6 +1,10 @@
 import sys
 
 from saying import hello
+from saying import goodbye
 
-if len(sys.argv)==2:
+if len(sys.argv)==3:
     hello(sys.argv[1])
+
+if len(sys.argv)==3:
+    goodbye(sys.argv[2])
