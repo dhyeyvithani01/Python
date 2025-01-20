0a850192e7d93b585162272dd626d48e20edbe51
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-20 13:58:24 +0530

    Update Built_inFun.py

--- Built_inFun.py
+++ Built_inFun.py
@@ -240,3 +240,26 @@ import sys
 
 # # for hiding the turtle
 # turtle.hideturtle()
+
+
+# # import the time module 
+# import time 
+
+# # define the countdown func. 
+# def countdown(t): 
+	
+# 	while t: 
+# 		mins, secs = divmod(t, 60) 
+# 		timer = '{:02d}:{:02d}'.format(mins, secs) 
+# 		print(timer, end="\r") 
+# 		time.sleep(1) 
+# 		t -= 1
+	
+# 	print('Fire in the hole!!') 
+
+
+# # input time in seconds 
+# t = input("Enter the time in seconds: ") 
+
+# # function call 
+# countdown(int(t)) 
