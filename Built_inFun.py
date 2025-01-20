1efbfe50e661b0e3f3c4d38a56176cd0abb724e4
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-20 16:23:58 +0530

    Update Built_inFun.py

--- Built_inFun.py
+++ Built_inFun.py
@@ -266,12 +266,12 @@ import sys
 # countdown(int(t)) 
 
 """requests"""
-import json
-import requests
-import sys
+# import json
+# import requests
+# import sys
 
-if len(sys.argv)!=2:
-    sys.exit()
+# if len(sys.argv)!=2:
+#     sys.exit()
 
 # response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+sys.argv[1])
 # print(json.dumps(response.json(),indent=2))
