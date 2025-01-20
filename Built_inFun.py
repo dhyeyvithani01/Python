7f903823e69ad113af976637990f7f12e68c6482
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-20 12:48:05 +0530

    Update Built_inFun.py

--- Built_inFun.py
+++ Built_inFun.py
@@ -158,8 +158,8 @@ import sys
 # """printing penguin using ASCII art"""
 # cowsay.tux("I am Tux the penguin!")
 
-
-"""import package"""
+"""Turtle"""
+##import package
 # import turtle
 # # loop for pattern
 # for i in range(4):
@@ -177,7 +177,7 @@ import sys
 #     turtle.left(180)
 
 
-"""import package""" 
+# #import package
 # import turtle
 # # loop for pattern 
 # for i in range(50): 
@@ -186,3 +186,57 @@ import sys
 # # motion for pattern 
 #     turtle.forward(50+10*i) 
 #     turtle.right(90)
+
+# # import package and making object
+# import turtle
+# screen = turtle.Screen()
+
+# # method to draw ellipse
+# def draw(rad):
+	
+# # rad --> radius for arc
+# 	for i in range(2):
+# 		turtle.circle(rad,90)
+# 		turtle.circle(rad//2,90)
+
+# # Main Section 
+# # Set screen size
+# screen.setup(500,500)
+
+# # Set screen color
+# screen.bgcolor('black')
+
+# # Colors
+# col=['violet','blue','green','yellow',
+# 	'orange','red']
+
+# # some integers
+# val=10
+# ind=0
+
+# # turtle speed
+# turtle.speed(10)
+
+# # loop for multiple ellipse
+# for i in range(36):
+	
+# 	# oriented the ellipse at angle = -val
+# 	turtle.seth(-val)
+	
+# 	# color of ellipse
+# 	turtle.color(col[ind])
+	
+# 	# to access different color
+# 	if ind==5:
+# 		ind=0
+# 	else:
+# 		ind+=1
+	
+# 	# calling method
+# 	draw(80)
+	
+# 	# orientation change
+# 	val+=10
+
+# # for hiding the turtle
+# turtle.hideturtle()
