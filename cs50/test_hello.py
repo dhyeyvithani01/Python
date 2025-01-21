b4768120003868017e2f15c67f38a588c563ffe8
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-21 15:38:01 +0530

    Update test_hello.py

--- cs50/test_hello.py
+++ cs50/test_hello.py
@@ -2,7 +2,9 @@ from hello import hello
 
 
 def test_default():
-    assert hello()=="Hello, world"
+    assert hello()=="Hello, World"
 
 def test_arguments():
     assert hello("Dhyey")=="Hello, Dhyey"
+
+hello("Dhyey")
