f56aa19b3b84bbb738203fa9a9c8b071e31e2ee5
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-21 16:30:24 +0530

    Create test_hello.py

---
+++ cs50/test/test_hello.py
@@ -0,0 +1,11 @@
+from hello import hello
+
+
+def test_default():
+    assert hello()=="Hello, World"
+
+def test_arguments():
+    assert hello("Dhyey")=="Hello, Dhyey"
+
+print(hello())
+print(hello("Dhyey"))
