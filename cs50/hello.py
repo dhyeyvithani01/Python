ffd227e3ac43c21badb15b24ed8a25577bc50af8
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-21 15:36:03 +0530

    Update hello.py

--- cs50/hello.py
+++ cs50/hello.py
@@ -1,9 +1,9 @@
 def main():
-    Name = input("What's your name ?\n")
-    print(hello(Name))
+    name = input("What's your name ? ")
+    print(hello(name))
 
 def hello(To="World"):
-    return f"Hello,{To}"
+    return f"Hello, {To}"
 
 if __name__ == "__main__":
     main()
