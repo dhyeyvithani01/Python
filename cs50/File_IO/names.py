a04ed03e1a8142dfc07c2a53711c45d414382027
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-22 15:58:22 +0530

    Update names.py

--- cs50/File_IO/names.py
+++ cs50/File_IO/names.py
@@ -1,5 +1,4 @@
-file=open("name.txt","w")
-for _ in range(3):
-    name = input("What's your name? ")
-    file.write(f"{name}\n")
+name = input("What's your name? ")
+file=open("name.txt","a")
+file.write(f"{name}\n")
 file.close()
