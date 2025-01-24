7e4b3dc0ecac64b3742e065dc992dff5e52a9d8a
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-24 09:48:32 +0530

    Delete Uno.py

--- Uno.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Welcome to the game of Uno!!!")
-    print("you can play in 1-4 players")
-    players = int(input("Choose the number of players:"))
-Colors=["Red","Blue","Green","Yellow"]
-Num=list(range(0,9))
-Actions = ["Reverse","Skip",""]
