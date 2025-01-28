a5d2eb92bde2c1c3cee15532ffdc8d9d1962e6ac
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-28 14:40:01 +0530

    Update names.py

--- cs50/File_IO/names.py
+++ cs50/File_IO/names.py
@@ -2,9 +2,14 @@ names=[]
 # name = input("What's your name? ")
 # with open("names.txt","a") as file:
 #     file.write(f"{name}\n")
-with open("names.txt","r") as file:
-    for line in file:
-        names.append(line.rstrip())
+"""method-1"""
+# with open("names.txt","r") as file:
+#     for line in file:
+#         names.append(line.rstrip())
 
-for i in sorted(names):
-    print(f"Hello, {i}")
+# for i in sorted(names):
+#     print(f"Hello, {i}")
+"""method-2"""
+with open("names.txt","r") as file:
+    for line in sorted(file):
+        print("Hello",line.rstrip())
