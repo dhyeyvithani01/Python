b71a3b15e5c0bef215ba6f9355132ed32bca16ae
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-01-28 14:29:50 +0530

    Update names.py

--- cs50/File_IO/names.py
+++ cs50/File_IO/names.py
@@ -2,6 +2,5 @@ name = input("What's your name? ")
 with open("names.txt","a") as file:
     file.write(f"{name}\n")
 with open("names.txt","r") as file:
-    lines = file.readlines()
-for line in lines:
-    print("Hello",line.rstrip())
+    for line in file:
+        print("Hello",line.rstrip())
