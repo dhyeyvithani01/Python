dc30fbf824f74feccad34ef4dd4d64d7f705696e
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-12 15:04:55 +0530

    Update names.py

--- cs50/File_IO/names.py
+++ cs50/File_IO/names.py
@@ -2,13 +2,13 @@
 # with open("names.txt","a") as file:
 #     file.write(f"{name}\n")
 """method-1"""
-names=[]                            #creating a list
-with open("names.txt","r") as file: #using 'with' so you don't have to use file.close()  
-    for line in file:                            
-        names.append(line.rstrip()) #appending list and adding text from names.txt file;line.rstrip() for deleting extra lines 
+# names=[]                            #creating a list
+# with open("names.txt","r") as file: #using 'with' so you don't have to use file.close()  
+#     for line in file:                            
+#         names.append(line.rstrip()) #appending list and adding text from names.txt file;line.rstrip() for deleting extra lines 
 
-for i in sorted(names):             #'sorted' for sorting names
-    print(f"Hello, {i}")
+# for i in sorted(names, reverse=True):             #'sorted' for sorting names and 'reverse=true' is used for reverse function of sorted
+#     print(f"Hello, {i}")
 """method-2"""
 # with open("names.csv","r") as file:
 #     for line in sorted(file,reverse=True):
