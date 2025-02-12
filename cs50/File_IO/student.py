17b8c1fcfc5923eab6c719414c00ab9d8671c6f8
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-12 16:11:04 +0530

    Update student.py

--- cs50/File_IO/student.py
+++ cs50/File_IO/student.py
@@ -6,11 +6,17 @@ with open("students.csv","r") as file:
         student ={"name":name,"house":house}
         students.append(student)
 
-def get_name(student):
-    return student["name"]
+# def get_name(student):
+#     return student["name"]
 
-def get_house(student):
-    return student["house"]
+# def get_house(student):
+#     return student["house"]
 
-for student in sorted(students,key=get_house,reverse=True):
+# for student in sorted(students,key=get_house,reverse=True): #here 'get house' is used without paranthesis because get_house is passed in sorted function
+#     print(f"{student['name']} is in {student['house']}")
+
+
+"""method-2 for sorting using lambda function"""
+
+for student in sorted(students,key=lambda student : student["name"]):
     print(f"{student['name']} is in {student['house']}")
