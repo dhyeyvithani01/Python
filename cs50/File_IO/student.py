789dc4f1949155d23e61130a927c69170104b29d
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-13 13:15:43 +0530

    Update student.py

--- cs50/File_IO/student.py
+++ cs50/File_IO/student.py
@@ -1,10 +1,10 @@
 students=[]
 
-with open("students.csv","r") as file:
-    for line in file:
-        name,house,home = line.rstrip().split(",")
-        student ={"name":name,"house":house,"home":home}
-        students.append(student)
+# with open("students.csv","r") as file:
+#     for line in file:
+#         name,house,home = line.rstrip().split(",")
+#         student ={"name":name,"house":house,"home":home}
+#         students.append(student)
 
 # def get_name(student):
 #     return student["name"]
@@ -20,3 +20,21 @@ with open("students.csv","r") as file:
 
 for student in sorted(students,key=lambda student : student["name","home"]):
     print(f"{student['name']} is in {student['house']}")
+
+
+import csv
+
+with open("students.csv","r") as file:
+    reader = csv.reader(file)
+    """method-1"""
+    # for row in reader:
+    #     students.append({"name":row[0],"house":row[1],"home":row[2]})
+    """method-2"""
+    for name,house,home in reader:
+        students.append({"name":name,"house":house,"home":home})
+
+for student in sorted(students,key=lambda student : student["name"]):
+    if student['home']!='Unknown':
+        print(f"{student['name']} is in {student['house']} and {student['name']} lives in {student['home']}")
+    else:
+        print(f"{student['name']} is in {student['house']} and information about {student['name']}'s home is Unknown.")
