4be902216166de3849a5b6aceb2a6f013a2c9286
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-20 16:52:54 +0530

    Update validate.py

--- cs50/Regex/validate.py
+++ cs50/Regex/validate.py
@@ -1,6 +1,6 @@
 import re
 
-email = input("What's your email?").strip()
+email = input("What's your email?\n").strip()
 
 if re.search(r"^(\w|\.)+@(\w|\.)+\.(edu|com|gov|in|us|au|org|net)$",email, re.IGNORECASE):    # \w = [a-zA-Z0-9_]
     print("vaild")
