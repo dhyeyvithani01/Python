9470ff8163e1fba648fe0b5b2fafb873451fb2fe
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-20 16:02:02 +0530

    Update validate.py

--- cs50/Regex/validate.py
+++ cs50/Regex/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email?").strip()
 
-if re.search(r"^\w+@\w+\.(edu|com|gov|in|us|aus|org|net)$",email):    # \w = [a-zA-Z0-9_]
+if re.search(r"^(\w|\.)+@\w+\.(edu|com|gov|in|us|aus|org|net)$",email, re.IGNORECASE):    # \w = [a-zA-Z0-9_]
     print("vaild")
 else:
     print("Invaild")
