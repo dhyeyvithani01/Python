219c84a60affaba93b39b6cf7d7d6812031bb7a0
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-20 15:21:48 +0530

    Update validate.py

--- cs50/Regex/validate.py
+++ cs50/Regex/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email?").strip()
 
-if re.search(r"^.+@.+\.edu$",email):
+if re.search(r"^\w+@\w+\.(edu|com|gov|in|us|aus|org|net)$",email):    # \w = [a-zA-Z0-9_]
     print("vaild")
 else:
     print("Invaild")
