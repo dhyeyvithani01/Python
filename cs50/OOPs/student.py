e529c96f9b8c557c77e52b9c1426fe5ff91267b0
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-02-27 16:13:50 +0530

    Update student.py

--- cs50/OOPs/student.py
+++ cs50/OOPs/student.py
@@ -1,11 +1,16 @@
+class Student:
+    def __init__(self,name="jenish",area="varachcha"):
+        self.name = name
+        self.area = area
+
 def main():
     student=get_student()
-    print(f"{student["name"]} from {student["house"]}")
+    print(f"{student.name} from {student.area}")
     
 def get_student():
     name = input("Name : ")
-    house = input("House : ")
-    return {"name": name ,"house":house}
+    area = input("Area : ")
+    return Student(name,area)
 
 if __name__ == "__main__":
     main()
