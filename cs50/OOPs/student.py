90a901175168bf4b694af2b0352be2fb53b5ef53
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-05 16:19:23 +0530

    Update student.py

--- cs50/OOPs/student.py
+++ cs50/OOPs/student.py
@@ -1,23 +1,37 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house,patronus):
         if not name:
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+    def charm(self):
+        match self.patronus:
+            case "stag":
+                return "stag"
+            case "otter":
+                return "otter"
+            case "jack":
+                return "jack"            
+            case _:
+                return "/"
+
 def main():
     student = get_student()
-    print(student)
+    print("Expecto patronum!")
+    print(student.charm())
 
 def get_student():
     name = input("Name: ") 
-    house = input("House: ")  
-    student = Student(name, house)
+    house = input("House: ") 
+    patronus = input("Patronus: ") 
+    student = Student(name,house,patronus)
     return student
 
 if __name__ == "__main__":
