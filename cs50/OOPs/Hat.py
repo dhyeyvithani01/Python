75f9406d6b230fe85cac92518894370a8688e4dd
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-07 15:34:48 +0530

    Create Hat.py

---
+++ cs50/OOPs/Hat.py
@@ -0,0 +1,11 @@
+import random
+class Hat:
+    def __init__(self):
+        self.houses =["Gryffindor","Ravenclaw","Hufflepuff","Slytherin"]
+    
+    def sort(self,name):
+        house = random.choice(self.houses)
+        print(name,"is in",house)
+
+hat = Hat()
+hat.sort("Harry")
