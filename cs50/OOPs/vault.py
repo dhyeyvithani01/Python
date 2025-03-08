f0f3d3ece632999f144943db8dec1d5fb9c7d47e
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-08 17:57:39 +0530

    Update vault.py

--- cs50/OOPs/vault.py
+++ cs50/OOPs/vault.py
@@ -4,5 +4,18 @@ class Vault:
         self.sickles = sickles
         self.knuts = knuts
         
+    def __str__(self):
+        return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+
 potter = Vault(100,50,25)
 print(potter)
+
+weasley = Vault(100,50,25)
+print(weasley)
+
+galleons = potter.galleons + weasley.galleons
+sickles = potter.sickles + weasley.sickles
+knuts = potter.knuts + weasley.knuts
+
+total = Vault(galleons,sickles,knuts)
+print(total)
