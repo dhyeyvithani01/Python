618223cad27327ed1c7c34e5ce404e1c8adfd358
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-09 12:03:23 +0530

    Update bank.py

--- cs50/etc/bank.py
+++ cs50/etc/bank.py
@@ -1,20 +1,45 @@
-balance = 0
+# balance = 0
 
+# def main():
+#     print("Balance:",balance)
+#     deposit(100)
+#     withdraw(50)
+#     print("Balance:",balance)
+
+
+# def deposit(n):
+#     global balance
+#     balance += n
+
+
+# def withdraw(n):
+#     global balance
+#     balance -= n
+
+# if __name__ == "__main__":
+#     main()
+
+
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self,n):
+        self._balance += n
+
+    def withdraw(self,n):
+        self._balance -=n
+    
 def main():
-    print("Balance:",balance)
-    deposit(100)
-    withdraw(50)
-    print("Balance:",balance)
-
-
-def deposit(n):
-    global balance
-    balance += n
-
-
-def withdraw(n):
-    global balance
-    balance -= n
-
+    account = Account()
+    print("Balance:",account.balance)
+    account.withdraw(50)
+    account.deposit(100)
+    print("Balance:",account.balance)
+    
 if __name__ == "__main__":
     main()
