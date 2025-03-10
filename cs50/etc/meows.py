d650d6b9e0c44029f7979734a5a3856fc290c28e
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-10 21:45:02 +0530

    Update meows.py

--- cs50/etc/meows.py
+++ cs50/etc/meows.py
@@ -19,7 +19,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="meow like a cat")
-parser.add_argument("-n", help="number of times to meow")
+parser.add_argument("-n",default=1,help="number of times to meow",type=int)
 args = parser.parse_args()
 
 for _ in range(int(args.n)):
