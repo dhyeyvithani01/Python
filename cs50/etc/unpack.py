ecad90383736c3ba77aeef6828f59a364602fffb
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-10 22:38:11 +0530

    Create unpack.py

---
+++ cs50/etc/unpack.py
@@ -0,0 +1,8 @@
+def total(galleons,sickles,knuts):
+    return (galleons*17+sickles)*29+knuts
+
+coins=(100,50,25)
+coin={"galleons":200,"sickles":100,"knuts":50}
+
+print(total(*coins),"Knuts")#* for unpacking list&
+print(total(**coin),"Knuts")#** for unpacking dictionary
