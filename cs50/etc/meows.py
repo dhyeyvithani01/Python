0be438f8289d7432e87112c85fef90f470c9d453
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-10 21:43:21 +0530

    Update meows.py

--- cs50/etc/meows.py
+++ cs50/etc/meows.py
@@ -1,16 +1,26 @@
-def meow(n: int) -> str:
-    """
-    Meows n times.
+# def meow(n: int) -> str:
+#     """
+#     Meows n times.
 
-    :param n: Number of times of meow
-    :type n: int
-    :raise TypeError: If n is not an int
-    :return: A string of n meows, one per line
-    :rtype: str
-    """
-    #this is a restructered text, markdown language used for documentation.
-    return "meow\n"*n
+#     :param n: Number of times of meow
+#     :type n: int
+#     :raise TypeError: If n is not an int
+#     :return: A string of n meows, one per line
+#     :rtype: str
+#     """
+#     #this is a restructered text, markdown language used for documentation.
+#     return "meow\n"*n
 
-number: int=int(input("Number: "))
-meows: str = meow(number)
-print(meows,end="")
+# number: int=int(input("Number: "))
+# meows: str = meow(number)
+# print(meows,end="")
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description="meow like a cat")
+parser.add_argument("-n", help="number of times to meow")
+args = parser.parse_args()
+
+for _ in range(int(args.n)):
+    print("Meow")
