6e6bba16a49e9dada04f2ec3e6ead1cea5bdb410
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-10 19:46:24 +0530

    Update meows.py

--- cs50/etc/meows.py
+++ cs50/etc/meows.py
@@ -1,4 +1,14 @@
 def meow(n: int) -> str:
+    """
+    Meows n times.
+
+    :param n: Number of times of meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
+    #this is a restructered text, markdown language used for documentation.
     return "meow\n"*n
 
 number: int=int(input("Number: "))
