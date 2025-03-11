2bad63f4c641bb6a5c991bf957dbb31542d8cfb6
dhyeyvithani01 <dhyey1121.7@gmail.com>
2025-03-11 18:41:13 +0530

    cs50 and programs

--- cs50/OOPs/Hat.py
+++
@@ -1,9 +0,0 @@
-import random
-class Hat:
-    houses =["Gryffindor","Ravenclaw","Hufflepuff","Slytherin"]
-    
-    @classmethod
-    def sort(cls,name):
-        print(name,"is in",random.choice(cls.houses))
-
-Hat.sort("Harry")
--- cs50/Regex/validate.py
+++
@@ -1,8 +0,0 @@
-import re
-
-email = input("What's your email?\n").strip()
-
-if re.search(r"^(\w|\.)+@(\w|\.)+\.(edu|com|gov|in|us|au|org|net)$",email, re.IGNORECASE):    # \w = [a-zA-Z0-9_]
-    print("vaild")
-else:
-    print("Invaild")
